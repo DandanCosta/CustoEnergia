1a7b905aa0cf9318c2187da1f9faa5eb363440bc
Danilo Costa <dandan_costa2222@outlook.com>
2025-03-31 18:45:46 -0300

    codigo do exercicio

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿double consumo, horasUsoDia;
+decimal custo, custoMes;
+
+Console.Write("- - - Custo de Energia - - -\n");
+
+Console.Write("Consumo do aparelho (em kWh/mes)...: ");
+consumo = double.Parse(Console.ReadLine()!);
+
+Console.Write("Horas de uso por dia...............: ");
+horasUsoDia = double.Parse(Console.ReadLine()!);
+
+Console.Write("Custo de Energia (em R$/kWh).......: ");
+custo = decimal.Parse(Console.ReadLine()!);
+
+custoMes = Convert.ToDecimal(consumo * horasUsoDia) * custo;
+
+Console.WriteLine($"\nCusto estimado: {custoMes:c2}");
